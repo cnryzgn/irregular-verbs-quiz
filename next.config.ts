2f0038215c7867505092eefff8a24612b095bb62
cnryzgn <yazgncaner@gmail.com>
2026-01-07 01:46:56 +0300

    fix: deploy.yml changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // GitHub Pages alt klasör yapısı için repo adını ekledik
-  basePath: '/irregular_verb_quiz',
   images: {
     unoptimized: true,
   },

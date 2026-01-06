606617220f465fb5d630c3ac5d16d9639657795e
cnryzgn <yazgncaner@gmail.com>
2026-01-07 01:28:53 +0300

    github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
+/** @type {import('next').Next} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Statik site oluşturmayı sağlar
+  basePath: process.env.BASE_PATH || '',
+  assetPrefix: process.env.BASE_PATH || '',
+  images: {
+    unoptimized: true, // GitHub Pages standart görsel optimizasyonunu desteklemez
+  },
 };
 
 export default nextConfig;
